04f7c55c1cb4c95b9a760b44a4d9861fbd7aa837
ignatov-ai <ignatov-ai@yandex.ru>
2023-07-10 10:27:27 +0300

    Добавлены обработчики действий на экране "Настройки"

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,12 +16,44 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val btn_back_to_main = findViewById<ImageView>(R.id.back_to_main)
-
-        btn_back_to_main.setOnClickListener{
+        val btnBackToMain = findViewById<ImageView>(R.id.backToMain)
+        btnBackToMain.setOnClickListener {
             val displayBack = Intent(this, MainActivity::class.java)
             startActivity(displayBack)
         }
 
+        val btnShare = findViewById<ImageView>(R.id.share)
+        btnShare.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Пройди курс разработчика Android приложений от Яндекс. Ссылка: https://practicum.yandex.ru/android-developer/"
+            )
+            startActivity(Intent.createChooser(shareIntent, "Share via"))
+        }
+
+        val btnSupport = findViewById<ImageView>(R.id.support)
+        btnSupport.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse("mailto:ignatov-ai@yandex.ru")
+            emailIntent.putExtra(
+                Intent.EXTRA_SUBJECT,
+                "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            )
+            emailIntent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Спасибо разработчикам и разработчицам за крутое приложение!"
+            )
+            startActivity(emailIntent)
+        }
+
+        val btnUserAgreement = findViewById<ImageView>(R.id.userAgreement)
+        btnUserAgreement.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW)
+            browserIntent.data =
+                Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            startActivity(browserIntent)
+        }
     }
 }
