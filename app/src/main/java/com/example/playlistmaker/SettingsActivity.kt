16ee0504f3eb7149b463333793044849b8e1e80b
ignatov-ai <ignatov-ai@yandex.ru>
2023-07-11 22:57:22 +0300

    Реализовал сохранение строки, которую успел ввести пользователь

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -5,10 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
 
 class SettingsActivity : AppCompatActivity() {
     @SuppressLint("WrongViewCast")
