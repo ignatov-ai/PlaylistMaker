f79469baf4258a2a711bf332b87ca97f76c18e53
ignatov-ai <ignatov-ai@yandex.ru>
2023-08-11 00:24:41 +0300

    Выполнил ДЗ из 12 спринта (переключатель темы день/ночь) в другом ПР
    Содал новую ветку для Спринта №12

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import java.text.SimpleDateFormat
-import java.util.Date
 import java.util.Locale
 
 class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
