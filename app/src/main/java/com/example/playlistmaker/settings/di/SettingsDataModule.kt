8daf8c2cca0b6d419a120496e7ea134a20b085a2
ignatov-ai <ignatov-ai@yandex.ru>
2023-11-14 02:35:38 +0300

    Спринт 17. Исправляю критические замечания: SharedPrefsDarkTheme нужно внедрить sharedPreferences в конструктор.

--- app/src/main/java/com/example/playlistmaker/settings/di/SettingsDataModule.kt
+++ app/src/main/java/com/example/playlistmaker/settings/di/SettingsDataModule.kt
@@ -10,7 +10,7 @@ import org.koin.dsl.module
 
 val settindsDataModule = module {
     single<DarkTheme> {
-        SharedPrefsDarkTheme(context = get())
+        SharedPrefsDarkTheme(sharedPrefs = get())
     }
 
     single<SharedPreferences> {
